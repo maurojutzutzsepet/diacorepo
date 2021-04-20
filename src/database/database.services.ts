ff663d10c0bfe93a17f6e2aa7aa972fe7547fe76
Mauro Jutzutz Sepet <mauro.jutzutz@gmail.com>
2021-04-19 22:40:41 -0600

    con scripdddt

--- src/database/database.services.ts
+++ src/database/database.services.ts
@@ -12,14 +12,23 @@ export const databaseProvider = [
       return {
         ssl: false,
         type: 'mssql' as 'mssql',
-        host: config.get(Configuration.HOST),
-        username: config.get(Configuration.USERNAME),
+        host: '104.154.189.53',
+        username: 'mjsusuario1',
         port: 1433,
-        password: config.get(Configuration.PASSWORD),
-        database: config.get(Configuration.DATABASE),
+        password: 'agudevdiaco1234',
+        database: 'testdb',
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         migrations: [__dirname + '/migrations/*{.ts,.js}'],
       } as ConnectionOptions;
     },
   }),
 ];
+
+// type: 'mssql' as 'mssql',
+//         host: config.get(Configuration.HOST),
+//         username: config.get(Configuration.USERNAME),
+//         port: 1433,
+//         password: config.get(Configuration.PASSWORD),
+//         database: config.get(Configuration.DATABASE),
+//         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+//         migrations: [__dirname + '/migrations/*{.ts,.js}'],
